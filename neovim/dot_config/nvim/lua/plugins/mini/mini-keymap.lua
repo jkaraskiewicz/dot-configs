74492281a31d485bc504bc67f2fb232243df7a90
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-06-09 10:01:10 +0200

    update

--- neovim/dot_config/nvim/lua/plugins/mini/mini-keymap.lua
+++ neovim/dot_config/nvim/lua/plugins/mini/mini-keymap.lua
@@ -3,5 +3,8 @@ return {
   version = false,
   config = function()
     require('mini.keymap').setup()
+
+    local map_multistep = require('mini.keymap').map_multistep
+    map_multistep('i', '<Tab>', { 'pmenu_accept' })
   end,
 }
