6443f96c2dadd898a1f4e3da57b0a8d28d0a2a0c
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-06-19 10:01:11 +0200

    neovim update

--- neovim/dot_config/nvim/lua/plugins/arrow.lua
+++ neovim/dot_config/nvim/lua/plugins/arrow.lua
@@ -9,7 +9,7 @@ return {
     buffer_leader_key = '<leader>Bb',
   },
   keys = {
-    { '<leader>Sf', function() require('arrow.ui').openMenu() end,        desc = 'File bookmarks' },
-    { '<leader>Sb', function() require('arrow.buffer_ui').openMenu() end, desc = 'Buffer bookmarks' },
+    { '<leader>Bf', function() require('arrow.ui').openMenu() end,        desc = 'File bookmarks' },
+    { '<leader>Bb', function() require('arrow.buffer_ui').openMenu() end, desc = 'Buffer bookmarks' },
   },
 }
