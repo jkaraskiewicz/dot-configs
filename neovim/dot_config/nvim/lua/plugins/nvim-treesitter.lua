7af70851efad4ed7a66f94428605dd2b02b6f9a6
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-06-30 09:51:05 +0200

    neovim update

--- neovim/dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -47,6 +47,40 @@ return {
       indent = {
         enable = false,
       },
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            [']m'] = { query = '@method.outer', desc = 'Next function' },
+            [']c'] = { query = '@call.*', desc = 'Next call' },
+            [']a'] = { query = '@parameter.inner', desc = 'Next argument' },
+          },
+          goto_previous_start = {
+            ['[m'] = { query = '@method.outer', desc = 'Previous function' },
+            ['[c'] = { query = '@call.*', desc = 'Previous call' },
+            ['[a'] = { query = '@parameter.inner', desc = 'Previous argument' },
+          },
+          goto_next_end = {
+            ['}m'] = { query = '@method.outer', desc = 'Next function' },
+            ['}c'] = { query = '@call.*', desc = 'Next call' },
+            ['}a'] = { query = '@parameter.inner', desc = 'Next argument' },
+          },
+          goto_previous_end = {
+            ['{m'] = { query = '@method.outer', desc = 'Previous function' },
+            ['{c'] = { query = '@call.*', desc = 'Previous call' },
+            ['{a'] = { query = '@parameter.inner', desc = 'Previous argument' },
+          },
+        },
+      },
     })
+
+    local repeatable_move = require('nvim-treesitter.textobjects.repeatable_move')
+
+    vim.keymap.set({ 'n', 'x', 'o' }, ']]', repeatable_move.repeat_last_move_next,
+      { noremap = true, silent = true, desc = 'Repeat last next move' })
+
+    vim.keymap.set({ 'n', 'x', 'o' }, '[[', repeatable_move.repeat_last_move_previous,
+      { noremap = true, silent = true, desc = 'Repeat last previous move' })
   end,
 }
