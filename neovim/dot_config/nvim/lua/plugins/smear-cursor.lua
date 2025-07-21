1853a112a3974d9769c62d707e5cdf47cb0f8330
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-07-21 12:49:30 +0200

    neovim update

---
+++ neovim/dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,12 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    smear_between_buffers = false,
+    smear_between_neighbor_lines = true,
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    damping = 0.67,
+    matrix_pixel_treshold = 0.5,
+    smear_to_cmd = false,
+  },
+}
