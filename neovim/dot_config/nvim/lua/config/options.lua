9dca873979d71f6fae97546ff052202054e28ec3
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-07-22 10:44:54 +0200

    neovim update

--- neovim/dot_config/nvim/lua/config/options.lua
+++ neovim/dot_config/nvim/lua/config/options.lua
@@ -68,20 +68,24 @@ end
 vim.g._ts_force_sync_parsing = true
 
 -- Neovide
-vim.g.neovide_cursor_trail_size = 0.0
-vim.g.neovide_cursor_animate_command_line = false
-vim.g.neovide_cursor_vfx_mode = { '', '' }
-vim.g.neovide_position_animation_length = 0.5
-vim.g.neovide_scroll_animation_length = 0.1
-vim.g.neovide_cursor_animation_length = 0.05
+if vim.g.neovide then
+  vim.opt.guifont = { 'CommitMono Nerd Font Mono', ':h16' }
 
-vim.g.neovide_scale_factor = 1.0
-local change_scale_factor = function(delta)
-  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  vim.g.neovide_cursor_trail_size = 0.0
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_vfx_mode = { '', '' }
+  vim.g.neovide_position_animation_length = 0.5
+  vim.g.neovide_scroll_animation_length = 0.1
+  vim.g.neovide_cursor_animation_length = 0.05
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set('n', '<C-=>', function()
+    change_scale_factor(1.1)
+  end)
+  vim.keymap.set('n', '<C-->', function()
+    change_scale_factor(1 / 1.1)
+  end)
 end
-vim.keymap.set('n', '<C-=>', function()
-  change_scale_factor(1.1)
-end)
-vim.keymap.set('n', '<C-->', function()
-  change_scale_factor(1 / 1.1)
-end)
