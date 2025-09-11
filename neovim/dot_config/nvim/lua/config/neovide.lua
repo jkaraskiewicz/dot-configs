3ecf2b52eb3924d9f80bb9260b181f632f1f0796
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-09-11 11:53:14 +0200

    dot-configs neovim update

--- neovim/dot_config/nvim/lua/config/neovide.lua
+++ neovim/dot_config/nvim/lua/config/neovide.lua
@@ -15,6 +15,7 @@ vim.g.neovide_scale_factor = 1.0
 
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  vim.cmd('mode | tabnew | tabclose')
 end
 
 vim.keymap.set('n', '<C-=>', function() change_scale_factor(1.1) end)
