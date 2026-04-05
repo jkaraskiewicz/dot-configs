93990779e89851897c85deab5d2becefecf82524
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2026-04-05 12:34:24 +0200

    Remove after queries as an attempt to simplify the config

--- neovim/dot_config/nvim/after/queries/abs/injections.scm
+++
@@ -1,8 +0,0 @@
-((system_command_call_content) @injection.content
-  (#set! injection.language "bash"))
-
-((exec_system_command_call_expression (single_quoted_string) @injection.content)
-  (#set! injection.language "bash"))
-
-((exec_system_command_call_expression (double_quoted_string) @injection.content)
-  (#set! injection.language "bash"))
